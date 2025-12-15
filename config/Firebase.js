81e066cb6d06aaa715e638e880490415356214d7
pakebismillah <nurmandityomoussa@gmail.com>
2025-12-15 12:47:23 +0700

    ubah cara baca firebase service json

--- config/Firebase.js
+++ config/Firebase.js
@@ -1,21 +1,11 @@
 import admin from "firebase-admin";
-import { readFileSync } from "fs";
-import { fileURLToPath } from "url";
-import path from "path";
 
-// Construct an absolute path to the service account file
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const serviceAccountPath = path.join(
-  __dirname,
-  "..",
-  "firebase",
-  "chai-ai-e2041-firebase-adminsdk-fbsvc-344079e4b1.json"
+// Ambil service account dari ENV (bukan file)
+const serviceAccount = JSON.parse(
+  process.env.FIREBASE_SERVICE_ACCOUNT
 );
 
-const serviceAccount = JSON.parse(readFileSync(serviceAccountPath, "utf8"));
-
-// Check if Firebase app is already initialized to prevent re-initialization
+// Cegah initialize ulang
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
